774df4f9684c038bbbdb121b89be5dcb12aab5df
ozanarkancan <can.ozanarkan@gmail.com>
2016-06-28 12:01:51 +0300

    gpu checker

--- src/GPUChecker.jl
+++ src/GPUChecker.jl
@@ -1,5 +1,25 @@
 module GPUChecker
 
 # package code goes here
+function gpu_usages()
+	rawtext = readall(pipeline(`nvidia-smi`, `grep |`, `grep N/A`))
+	str_usages = split(rawtext, '\n')
+	usages = []
+	for usg in str_usages
+		if usg != ""
+			usage, capacity = map(x->parse(Int, match(r"(\d+)", x).captures[1]),split(strip(split(usg, '|')[3]), '/'))
+			push!(usages, (usage, capacity))
+		end
+	end
+	return usages
+end
+
+function first_min_used_gpu()
+	usages = gpu_usages()
+	return indmin(usages) - 1
+end
+
+export gpu_usages
+export first_min_used_gpu
 
 end # module
